7b9b7660ab692cea852b5be6b7592f718d4d082d
Adam Harvey <heyitsmeharv@gmail.com>
2023-05-09 19:09:07 +0100

    added text to the start screen

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -2,6 +2,14 @@ import React from "react";
 import "./styles.css";
 
 const Start = ({ playerName, handlePlayerName, handleStartGame }) => {
+  const text = [
+    'Hello, and welcome to this text based adventure.',
+    'Do you have what it takes?',
+    'Please lower all expectations.',
+    'Beware, Spooky Ghost!',
+    'Can you navigate all the way to the end?'
+  ];
+
   return (
     <>
       <div className="rain-container">
@@ -18,6 +26,9 @@ const Start = ({ playerName, handlePlayerName, handleStartGame }) => {
       </div>
       <div className="title-container">
         <h1 className="title"> Text Based Adventure</h1>
+        <div className="typewriter">
+          <h1>{text[Math.floor(Math.random() * text.length)]}</h1>
+        </div>
         <input className="input" placeholder="Enter Your Name" value={playerName} onChange={(e) => handlePlayerName(e)} />
         <button className="button" onClick={handleStartGame}>Enter</button>
       </div>
