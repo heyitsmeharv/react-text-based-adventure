6d0aaf314c4d906160ef985579a8243d384976df
Adam Harvey <heyitsmeharv@gmail.com>
2023-05-10 02:28:11 +0100

    room styling.

--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -5,22 +5,28 @@ const Room = ({ room, description, items, inventory, onInteract, onNavigate }) =
   console.log('room', room);
   return (
     <div className='room-container'>
-      <h1>{room.name}</h1>
-      <span className="room-text">{description}</span>
-      {items.length > 0 && (
-        <div>
-          <p className="room-text">Items in the room:</p>
-          <ul>
-            {items.filter(item => !inventory.includes(item)).map((item, index) => (
-              <li key={index}>
-                {item.name} ({item.description})
-                <button className="room-button" onClick={() => onInteract(item)}>Pick up</button>
-              </li>
-            ))}
-          </ul>
+      <div className='room-scenario'>
+        <h1>{room.name}</h1>
+        <span className="room-text">{description}</span>
+        <div className='room-items'>
+          {items.length > 0 && (
+            <div>
+              <h1 className="room-text-header">Items in the room:</h1>
+              <ul>
+                {items.filter(item => !inventory.includes(item)).map((item, index) => (
+                  <li key={index}>
+                    <span className="room-text">{item.name} - ({item.description})</span>
+                    <button className="room-button-item" onClick={() => onInteract(item)}>Pick up</button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
-      )}
-      <button className="room-button" onClick={onNavigate}>Look around</button>
+      </div>
+      <div className='room-options'>
+        <button className="room-button" onClick={onNavigate}>Look around</button>
+      </div>
     </div>
   );
 };
